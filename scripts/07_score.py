16361cd920cfc7d6741d2ea4384948d305340686
stephta <stephanieta@hotmail.ca>
2024-12-03 22:37:38 -0800

    add code from notebook, read in objects and save objects as necessary

--- scripts/07_score.py
+++ scripts/07_score.py
@@ -16,7 +16,24 @@ import pickle
 @click.option('--logreg-model-from', type=str, help="Path to directory where the logistic regression model is")
 @click.option('--scores-to', type=str, help="Path to directory where the score will be written to")
 def main(dummy_model_from, logreg_model_from, scores_to):
-    
+
+    # read in saved dummy model object
+    with open(dummy_model_from, 'rb') as f:
+        dummy_classifier = pickle.load(f)
+
+    # read in saved logistiv regression model object
+    with open(logreg_model_from, 'rb') as f:
+        random_search = pickle.load(f)
+
+    # score dummy and best logistic regression model on test set
+    dummy_test_score = dummy_classifier.score(X_test, y_test)
+    log_reg_test_score = random_search.score(X_test, y_test)
+
+    # save the test accuracy scores
+    model_results = pd.read_csv(scores_to)
+    model_results['test_accuracy'][0] = dummy_test_score
+    model_results['test_accuracy'][1] = log_reg_test_score
+    model_results.to_csv(scores_to)
 
 if __name__ == "__main__":
     main()
