88ca2592426e0242bd231c677e314c73a418819e
stephta <stephanieta@hotmail.ca>
2024-12-04 22:19:00 -0800

    add documentation for main function in script

--- scripts/06_tune_and_train.py
+++ scripts/06_tune_and_train.py
@@ -8,7 +8,7 @@
 # It also saves that mean cross validation accuracy score.
 #
 # Usage: python scripts/06_tune_and_train.py \
-# --X-train-data=data/processed/X_train.csv \
+# --x-train-data=data/processed/X_train.csv \
 # --y-train-data=data/processed/y_train.csv \
 # --model-to=results/models/logreg_classifier.pickle \
 # --scores-to=results/tables/model_scores.csv
@@ -21,20 +21,27 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import RandomizedSearchCV
 from scipy.stats import loguniform
 import pickle
+import click
 
-@click.option('--X-train-data', type=str, help="Path to X training data (features only)")
+@click.command()
+@click.option('--x-train-data', type=str, help="Path to X training data (features only)")
 @click.option('--y-train-data', type=str, help="Path to y training data (target only)")
 @click.option('--model-to', type=str, help="Path to directory where the model will be written to")
 @click.option('--scores-to', type=str, help="Path to directory where the score will be written to")
-def main(X_train_data, y_train_data, model_to, scores_to):
+def main(x_train_data, y_train_data, model_to, scores_to):
+    """
+    Trains and tunes the logistic regression model using RandomSearchCV,
+    then saves the model with the hyperparameters that gives the best mean cross validation accuracy.
+    Also saves that mean cross validation accuracy score.
+    """
     # read in data
-    X_train = pd.read_csv(X_train_data)
+    X_train = pd.read_csv(x_train_data)
     y_train = pd.read_csv(y_train_data)
 
     # lists of each type of feature
-    numeric_cols = ['Administrative', 'Administrative_Duration',
-                    'Informational', 'Informational_Duration',
-                    'ProductRelated', 'ProductRelated_Duration',
+    numeric_cols = ['Administrative_Duration',
+                    'Informational_Duration',
+                    'ProductRelated_Duration',
                     'BounceRates', 'ExitRates',
                     'PageValues', 'SpecialDay']
     categorical_cols = ['Weekend', 'OperatingSystems',
