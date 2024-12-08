20cfc1fdba25b7940cb2e1a89220c17c4ae0ce72
jdaduica <juliandad2001@hotmail.com>
2024-12-07 18:29:27 -0800

    updating descritions for py scripts 1-3

--- scripts/01_extract_data.py
+++ scripts/01_extract_data.py
@@ -1,9 +1,16 @@
 # extract_data.py
-# Author: Julian Daduica, Stephanie Ta, WaiMing Wong
+# Author: Julian Daduica
 # Date: 2024-12-03
 
-from ucimlrepo import fetch_ucirepo # raw data is from this package
+# This script gets the "Online Shoppers Purchasing Intention Dataset" from the UCI ML repository,
+# saves it as a CSV file, and validates the data schema.
+#
+# Usage: python scripts/01_extract_data.py \
+# --write_to=data/raw/
 
+
+
+from ucimlrepo import fetch_ucirepo # raw data is from this package
 import click
 import os
 import requests
@@ -21,6 +28,20 @@ def check_directory_exists(directory):
 @click.option('--write_to', type = str, help = "Path to directory where raw data will be written to")
 def main(write_to):
 
+    """
+    Fetches the "Online Shoppers Purchasing Intention Dataset" from the UCI ML repository, saves it as a CSV file, and
+    validates the data schema.
+
+    Parameters:
+    ----------
+    write_to : str
+        The directory where the CSV file will be written to. Must be an existing directory.
+
+    Returns:
+    -------
+    None
+
+    """
     check_directory_exists(write_to)
     
     online_shoppers_purchasing_intention_dataset = fetch_ucirepo(id=468) 
@@ -32,7 +53,8 @@ def main(write_to):
 
 
     # validate that the raw data was saved as a csv file
-    #assert os.path.isfile(write_to)
+    file_path = os.path.join(write_to, "raw_data.csv")
+    assert os.path.isfile(file_path), f"File {file_path} was not created."
 
     # validate data
     schema = pa.DataFrameSchema(
